ecf6170dbfb26b23526a0b0fa0e919c2fd4cdb24
el-2001 <eligla2001@outlook.com>
2022-07-07 18:45:41 -0500

    added responsiveness in title home

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,17 +1,33 @@
 
 import './Home.css';
 import homeIceCream from '../../Pictures/homeIceCream.jpg';
-
+import { useEffect , useState} from 'react';
+import iceCreamHome from '../../Pictures/iceCreamHome.svg';
 export const Home = ()=>{
+  
+  const [matches, setMatches]  = useState(
+    window.matchMedia("(max-width: 556px)").matches
+  )
+   
+  useEffect(() => {
+    window
+    .matchMedia("(max-width: 556px)")
+    .addEventListener('change', e => setMatches( e.matches ));
+  }, []);
+    
     return(<div className="Home top-space">
         <article className="no-padding">
-  <img className="responsive medium" src={homeIceCream}/>
-  <div className="absolute bottom left right padding bottom-shadow white-text">
-    <h2 className="center-align yellow10 ">New Flavors</h2>
-    <h5 className="center-align yellow10">Every Week</h5>
-   
+  <img className="responsive" src={homeIceCream}/>
+  <div className="responsive absolute top left right padding  white-text">
+    {matches && (<div><h3 className="center-align yellow10 small">New Flavors</h3>
+    <h4 className="center-align yellow10">Every Week</h4></div>)}
+    {!matches && (<div><h1 className="center-align yellow10 small">New Flavors</h1>
+    <h3 className="center-align yellow10">Every Week</h3></div>)}
   </div>
+  
+
 </article>
+
 
     </div>)
 }
